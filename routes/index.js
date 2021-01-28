738e799ec85fdae121a1bc9bceafd82fcfaedce2
Nicolas POYET <nicolas.poyet@gmail.com>
2021-01-28 11:02:18 +0100

    journeySchema on journeys.js

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var journeySchema = mongoose.Schema({
-  departure: String,
-  arrival: String,
-  date: Date,
-  departureTime: String,
-  price: Number,
-});
-
-var journeyModel = mongoose.model('journey', journeySchema);
+var journeyModel = require('../models/journeys')
 
 var city = ["Paris","Marseille","Nantes","Lyon","Rennes","Melun","Bordeaux","Lille"]
 var date = ["2018-11-20","2018-11-21","2018-11-22","2018-11-23","2018-11-24"]
