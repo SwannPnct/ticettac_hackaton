108c1d41d122c75890bd123b44fb104615b3b10b
Swann Pincet <swann.pincet@gmail.com>
2021-01-28 16:29:24 +0100

    added status on journeyschema

--- models/journeys.js
+++ models/journeys.js
@@ -6,6 +6,7 @@ var journeySchema = mongoose.Schema({
     date: Date,
     departureTime: String,
     price: Number,
+    status: String,
   });
   
   var journeyModel = mongoose.model('journey', journeySchema);
--- routes/index.js
+++ routes/index.js
@@ -95,6 +95,13 @@ router.get('/book-ticket', async (req,res,next) => {
   res.render('tickets', {bookings: user.bookings})
 })
 
+router.get('/last-trips', async (req,res,next) => {
+  const trips = await UserModel.findById("6012c4f711d6ec4ff405116e").populate('bookings');
+  console.log("here");
+  console.log(trips);
+  res.render('lasttrips',{})
+})
+
 
 
 module.exports = router;
