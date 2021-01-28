ee725e629c8c6afc8cecde936088c1fcf22900fc
Swann Pincet <swann.pincet@gmail.com>
2021-01-28 12:51:27 +0100

    created core sign-up/sign-in process

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,51 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
 
-/* GET users listing. */
+const UserModel = require('../models/users');
+
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.redirect('users/login');
 });
+
+router.get('/login', (req,res,next) => {
+  res.render('login', {});
+})
+
+router.post('/sign-up', async (req,res,next) => {
+
+  const checkEmail = await UserModel.findOne({email: req.body.email});
+
+  if (checkEmail) {
+    console.log("email already in use");
+    res.redirect('/users/login');
+    return;
+  }
+
+  const newUser = new UserModel({
+    firstName : req.body.firstName,
+    lastName : req.body.lastName,
+    email : req.body.email,
+    password : req.body.password,
+  })
+
+  await newUser.save();
+  console.log("user added!");
+
+  res.render('index',{});
+})
+
+router.post('/sign-in', async (req,res,next) => {
+  const check = await UserModel.findOne({email: req.body.email, password: req.body.password});
+
+  if (check) {
+    console.log("user connected!");
+    res.render('index', {})
+  } else {
+    console.log("wrong credentials or user not existing");
+    res.redirect('/users/login');
+  }
+})
 
 module.exports = router;
