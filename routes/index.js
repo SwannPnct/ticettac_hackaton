3ea602b014cd7873e88916477be9c3f766dd4feb
Nicolas POYET <nicolas.poyet@gmail.com>
2021-01-29 11:52:08 +0100

    arrival and departure case handled when journey search

--- routes/index.js
+++ routes/index.js
@@ -17,11 +17,13 @@ router.get('/', async function(req, res, next) {
 /* POST search page. */
 router.post('/search', async function(req, res, next) {
 
-// sécurité à ajouter: mettre tout en minuscule et première lettre en majuscule sur les req.body
+departureFormatted = req.body.departure.charAt(0).toUpperCase() + req.body.departure.toLowerCase().slice(1);
+arrivalFormatted = req.body.arrival.charAt(0).toUpperCase() + req.body.arrival.toLowerCase().slice(1);
+
 
   var result = await journeyModel.find({
-    departure:req.body.departure,
-    arrival:req.body.arrival,
+    departure:departureFormatted,
+    arrival:arrivalFormatted,
     date:req.body.date
   }) 
 
